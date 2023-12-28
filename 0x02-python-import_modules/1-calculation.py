20e10541b7cb67040b48838b15574cdb3b26cd07
nooby6 <giftaudrine1@gmail.com>
2023-12-28 04:02:13 -0800

    program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
 
+# Check if the script is run directly
 if __name__ == "__main__":
+    
+    # Define variables
     a = 10
     b = 5
     
+    # Import necessary functions from calculator_1 module
     from calculator_1 import add, sub, mul, div
     
+    # Perform addition and store the result
     result_add = add(a, b)
+    
+    # Perform subtraction and store the result
     result_sub = sub(a, b)
+    
+    # Perform multiplication and store the result
     result_mul = mul(a, b)
+    
+    # Perform division and store the result
     result_div = div(a, b)
     
+    # Print the results using the specified format
     print("{} + {} = {}".format(a, b, result_add))
     print("{} - {} = {}".format(a, b, result_sub))
     print("{} * {} = {}".format(a, b, result_mul))
