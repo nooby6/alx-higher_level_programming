0005077490cf9935131e95673f447552cc7c558f
nooby6 <giftaudrine1@gmail.com>
2024-01-07 08:03:30 -0800

    main.h

---
+++ 0x07-python-test_driven_development/main.h
@@ -0,0 +1,15 @@
+#define MAIN_H
+#ifndef MAIN_H
+
+/*These are my prototypes*/
+def add_integer(a, b=98):
+def matrix_divided(matrix, div):
+def say_my_name(first_name, last_name=""):
+def print_square(size):
+def text_indentation(text):
+def max_integer(list=[]):
+void print_python_string(PyObject *p);
+def lazy_matrix_mul(m_a, m_b):
+def matrix_mul(m_a, m_b):
+
+#endif
