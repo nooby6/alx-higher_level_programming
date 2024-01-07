bfc6adf6295181a0d037a0f1e4c143af704f7c0f
nooby6 <giftaudrine1@gmail.com>
2024-01-07 04:03:00 -0800

    a function magic_string() that returns a string “BestSchool” n times the number of the iteration (see code):

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,5 @@
+73% of storage used … If you run out, you won't have enough storage to create, edit, and upload files. Get 100 GB of storage for Ksh 250.00 Ksh 60.00/month for 3 months.
+#!/usr/bin/python3
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
