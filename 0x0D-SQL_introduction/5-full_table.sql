f1d1404bfa257906563c24d302a48f016b878302
nooby6 <giftaudrine1@gmail.com>
2024-02-14 11:12:55 -0800

    a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,20 +1,2 @@
-#!/bin/bash
-
-# Script to print the full description of the table first_table from the database in MySQL server
-# Usage: ./5-print_table_description.sh <database_name>
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-DB_NAME="$1"
-TABLE_NAME="first_table"
-
-# Define the SQL query to get the table description
-SQL_QUERY="SELECT COLUMN_NAME, COLUMN_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = '$DB_NAME' AND TABLE_NAME = '$TABLE_NAME';"
-
-# Execute the SQL query to get the table description
-mysql -uroot -proot "$DB_NAME" -e "$SQL_QUERY"
+-- Script to display the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+DESCRIBE hbtn_0c_0.first_table;
