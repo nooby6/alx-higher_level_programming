4764d641b4b864a30cdfde288298de51584005b7
nooby6 <giftaudrine1@gmail.com>
2024-02-14 10:51:05 -0800

    a script that creates a table called first_table in the current database in your MySQL server.

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,21 +1,8 @@
-#!/bin/bash
-
-# Script to create a table called first_table in the current database in MySQL server
-# Usage: ./4-create_table.sh <database_name>
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-DB_NAME="$1"
-
-# Define the SQL query to create the table
-SQL_QUERY="CREATE TABLE IF NOT EXISTS first_table (
+-- Script to create a table called first_table in the current database in your MySQL server.
+-- first_table description:
+-- id INT
+-- name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
-);"
-
-# Execute the SQL query to create the table
-mysql -uroot -proot "$DB_NAME" -e "$SQL_QUERY"
-
+);
