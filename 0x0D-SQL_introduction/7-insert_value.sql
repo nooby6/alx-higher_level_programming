64e08068fdcbbc932e032ca560d25b374b1c5b0b
nooby6 <giftaudrine1@gmail.com>
2024-02-14 11:18:38 -0800

    a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,21 +1,6 @@
-#!/bin/bash
-
-# Script to insert a new row into the table first_table in the database in MySQL server
-# Usage: ./7-insert_row.sh <database_name>
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-DB_NAME="$1"
-TABLE_NAME="first_table"
-ID=89
-NAME="Best School"
-
-# Define the SQL query to insert a new row into the table
-SQL_QUERY="INSERT INTO $TABLE_NAME (id, name) VALUES ($ID, '$NAME');"
-
-# Execute the SQL query to insert the new row into the table
-mysql -uroot -proot "$DB_NAME" -e "$SQL_QUERY"
+-- Script to insert a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+-- New row:
+-- id = 89
+-- name = Best School
+INSERT INTO hbtn_0c_0.first_table (id, name) VALUES (89, 'Best School');
 
