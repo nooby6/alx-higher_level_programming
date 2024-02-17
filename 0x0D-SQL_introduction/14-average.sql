018bef9eb543914cf485cbf4d8965adbc29d6c15
nooby6 <giftaudrine1@gmail.com>
2024-02-17 06:06:02 -0800

    a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,19 +1,3 @@
-#!/bin/bash
-
-# Script to compute the average score of all records in the table second_table in the database in MySQL server
-# Usage: ./compute_average_score.sh <database_name>
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-DB_NAME="$1"
-TABLE_NAME="second_table"
-
-# Define the SQL query to compute the average score
-SQL_QUERY="SELECT AVG(score) AS average FROM $TABLE_NAME;"
-
-# Execute the SQL query to compute the average score
-mysql -uroot -proot "$DB_NAME" -e "$SQL_QUER"
-
+-- Computes the average score of all records in the table second_table in my MySQL server.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
