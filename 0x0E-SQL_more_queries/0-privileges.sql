b8ca20ebfb8c1997712f401000a772114602e89f
nooby6 <giftaudrine1@gmail.com>
2024-02-17 00:59:26 -0800

    a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,19 +1,3 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
---Script: list_privileges.sh
---Description: Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on the localhost.
-
---Function to list privileges for a MySQL user
-list_privileges() {
-    local username="$1"
-    local hostname="$2"
-    # SQL query to show grants for the user
-    mysql -h localhost -u root -p -e "SHOW GRANTS FOR '$username'@'$hostname';"
-}
-
---List privileges for user_0d_1
-echo "Privileges for user_0d_1:"
-list_privileges "user_0d_1" "localhost"
-
---List privileges for user_0d_2
-echo "Privileges for user_0d_2:"
-list_privileges "user_0d_2" "localhost"
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
