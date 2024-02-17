e36d88b4001006f69e4d7f606e38694bd331a5aa
nooby6 <giftaudrine1@gmail.com>
2024-02-17 06:02:47 -0800

    a script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,19 +1,3 @@
-#!/bin/bash
-
-# Script to remove all records with a score <= 5 from the table second_table in the database in MySQL server
-# Usage: ./remove_low_scores.sh <database_name>
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-DB_NAME="$1"
-TABLE_NAME="second_table"
-
-# Define the SQL query to remove all records with score <= 5
-SQL_QUERY="DELETE FROM $TABLE_NAME WHERE score <= 5;"
-
-# Execute the SQL query to remove all records with score <= 5
-mysql -uroot -proot "$DB_NAME" -e "$SQL_QUERY"
-
+-- Removes all records with a score <= 5 in the table second_table in my MySQL server.
+DELETE FROM `second_table`
+WHERE `score` <= 5;
