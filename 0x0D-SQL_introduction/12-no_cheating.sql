535c21d791dc5e5e882b9984c862561e4f39c928
nooby6 <giftaudrine1@gmail.com>
2024-02-17 05:59:46 -0800

    a script that updates the score of Bob to 10 in the table second_table.

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,17 +1,4 @@
-#!/bin/bash
-
-# Script to update the score of Bob to 10 in the table second_table of the database in MySQL server
-# Usage: ./12-update_score.sh <database_name>
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-DB_NAME="$1"
-TABLE_NAME="second_table"
-NAME="Bob"
-NEW_SCORE=10
-
-# Define the SQL query to update the
-
+-- Updates the score of Bob to 10 in the table second_table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
