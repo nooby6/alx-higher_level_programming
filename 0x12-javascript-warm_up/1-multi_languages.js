212550510fb10c047cc18518a966b9b85ccccc11
nooby6 <giftaudrine1@gmail.com>
2024-03-12 03:35:49 -0700

    a function the prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Print the first line
+console.log('C is fun');
+
+// Print the second line
+console.log('Python is cool');
+
+// Print the third line
+console.log('JavaScript is amazing');
+
