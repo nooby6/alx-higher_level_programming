536a65c1cf28af6c5fda467ffa6a1966f29a2c81
Audrine Whrite <giftaudrine1@gmail.com>
2024-05-23 09:46:26 -0700

    script that writes a string to a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,18 @@
 #!/usr/bin/node
 
+// Import the fs module to handle file system operations
 const fs = require('fs');
 
 // Get the file path from the first command line argument
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
+// Read the file content in utf-8 encoding
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
     // Print the error object if an error occurred
-    console.log(err);
-  } else {
-    // Print the file content
-    console.log(data);
+    console.error(error);
+    return;
   }
+  // Print the file content
+  console.log(data);
 });
